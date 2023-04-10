76ef0f469a1f69bb33170a86a19a03e0e0732e8a
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-10 14:09:52 +0100

    add task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,9 +23,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
-		new_node->right->parent = new;
+		new_node->right->parent = new_;
 	}
 
-	parent->right = new_node
+	parent->right = new_node;
 	return (new_node);
 }
