68d87bc60af8ee4dda1e68d12905ab4b2429a831
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-10 14:13:22 +0100

    hot fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
-		new_node->right->parent = new_;
+		new_node->right->parent = new_node;
 	}
 
 	parent->right = new_node;
