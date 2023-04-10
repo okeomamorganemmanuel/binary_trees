ac8b5ad215f7fdfbf71e8be59306708cda6a80d8
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-04-10 18:35:20 +0100

    add task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,4 +1,4 @@
-#include "bibary_trees.h"
+#include "binary_trees.h"
 
 
 /**
@@ -12,6 +12,6 @@ void binary_tree_delete(binary_tree_t *tree)
 	{
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
-		free(tree)
+		free(tree);
 	}
 }
